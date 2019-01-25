5073576e7a700fc91bb98ee842228cb27068d04b
Bayu-Pamungkas <bayupamungkas0591@gmail.com>
2019-01-25 10:33:44 +0700

    Commit Pertama

--- HitungLuas.java
+++ HitungLuas.java
@@ -1,6 +1,22 @@
 import java.util.Scanner;
 public class HitungLuas;
 Scanner s = new Scanner(System.in);
+publi HitungLuas(){
+  cariLuasPersegiPanjang();
+  cariLuasLingkaran();
+}
+
+public void cariLuasPersegiPanjang(){
+  System.out.println("Menghitung Luas Persegi Panjang");
+  System.out.print("Masukkan Panjang: ");
+  int panjang = scan.nextInt();
+  System.out.print("Masukkan Lebar: ");
+  int lebar = scan.nextInt();
+
+  int luasPersegiPanjang = panjang * lebar;
+  System.out.println("Luas Persegi Panjang=" +luas);
+  System.out.println("luasPersegiPanjang");
+}
 public static void main(String[] args) {
 
 }
