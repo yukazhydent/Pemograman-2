c5931f50ee363a8f400f141737cd4742d1a54677
Bayu-Pamungkas <bayupamungkas0591@gmail.com>
2019-01-25 16:07:10 +0700

    Perubahan keduabelas

--- HitungLuas.java
+++ HitungLuas.java
@@ -4,6 +4,7 @@ public class HitungLuas {
 
   public HitungLuas(){
     cariLuasPersegiPanjang();
+    cariLuasLingkaran();
   }
 
   public void cariLuasPersegiPanjang(){
@@ -16,6 +17,15 @@ public class HitungLuas {
     int luasPersegiPanjang = panjang * lebar;
     System.out.println("Berikut luas Persegi Panjang dengan panjang = " + panjang + " dan lebar = " + lebar + ":");
     System.out.println(luasPersegiPanjang);
+  }
+  public void cariLuasLingkaran(){
+    System.out.println("Kita akan menghitung luas lingkaran!");
+    System.out.println("Masukan nilai dari Jari-jari:");
+    int jari = s.nextInt();
+
+    double luasLingkaran = (double)Math.pow(jari, 2) * 3.14;
+    System.out.println("Berikut luas Lingkaran dengan  Jari-jari = " + jari + ":");
+    System.out.println(luasLingkaran);
   }
 public static void main(String[] args) {
   new HitungLuas();
