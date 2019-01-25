2917cd0a49176a790829eaa0c50ee7125ec8fb4d
Bayu-Pamungkas <bayupamungkas0591@gmail.com>
2019-01-25 15:27:34 +0700

    Perubahan Kesepuluh

--- HitungLuas.java
+++ HitungLuas.java
@@ -2,7 +2,19 @@ import java.util.Scanner;
 public class HitungLuas{
   Scanner s = new Scanner(System.in);
   public HitungLuas(){
+  cariLuasPersegiPanjang();
   cariLuasLingkaran();
+}
+public void cariLuasPersegiPanjang(){
+  System.out.println("Kita akan menghitung luas Persegi Panjang!");
+  System.out.println("Masukan nilai dari Panjang:");
+  int panjang = s.nextInt();
+  System.out.println("Masukan nilai dari Lebar:");
+  int lebar = s.nextInt();
+
+  int luasPersegiPanjang = panjang * lebar;
+  System.out.println("Berikut luas Persegi Panjang dengan panjang = " + panjang + " dan lebar = " + lebar + ":");
+  System.out.println(luasPersegiPanjang);
 }
   public void cariLuasLingkaran(){
       System.out.println("Kita akan menghitung luas lingkaran!");
